f8055670bcbc5c9327a5d26d6821319aac817086
Pushendra2020 <pushendravedak2020@gmail.com>
2024-11-19 12:29:28 +0530

    update 1

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+const app = express();
+const port = 3000;
+//import mongoose from 'mongoose';
+import connectDB from './db/connect.js';
+const start = async () => {
+    try{
+        await connectDB();
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`)
+        });
+    }catch(error){
+        console.log(error)
+    }
+};
+start();
